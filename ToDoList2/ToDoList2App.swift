73ecac8e11386e6a13440b2c98738433da6eb12e
Tiffany Liu <mazingmonkey2004@gmail.com>
2023-08-21 14:32:44 -0400

    Initial Commit

---
+++ ToDoList2/ToDoList2App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList2App.swift
+//  ToDoList2
+//
+//  Created by Scholar on 8/21/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
